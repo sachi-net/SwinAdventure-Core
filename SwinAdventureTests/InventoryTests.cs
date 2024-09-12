f26f4353ae88311452abf91279f33dab1721aeb2
Sachintha Silva <netsachintha@outlook.com>
2024-09-13 01:54:38 +0530

    Implement Task 4.2 Iteration 2: Players, Items, and Inventory

---
+++ SwinAdventureTests/InventoryTests.cs
@@ -0,0 +1,69 @@
+﻿using SwinAdventureLibrary;
+
+namespace SwinAdventureTests;
+
+[TestFixture]
+public class InventoryTests
+{
+    Inventory inventory;
+
+    [SetUp]
+    public void Setup()
+    {
+        inventory = new();
+        List<Item> items = new()
+        {
+            new(new[]{ "shovel" }, "a shovel", "utility tool"),
+            new(new[]{ "sword" }, "a sword", "closed combat weapon"),
+            new(new[]{ "pc" }, "a small computer", "electronic device")
+        };
+
+        foreach (Item item in items)
+        {
+            inventory.Put(item);
+        }
+    }
+
+    [Test(Description = "The Inventory has items that are put in it")]
+    public void TestFindItem()
+    {
+        bool itemExists = inventory.HasItem("sword");
+        Assert.That(itemExists, Is.True);
+    }
+
+    [Test(Description = "The Inventory does not have items it does not contain")]
+    public void TestNoItems ()
+    {
+        inventory = new();
+        bool result = inventory.HasItem("bow");
+        Assert.That(result, Is.False);
+    }
+
+    [Test(Description = "Returns items it has, and the item remains in the inventory")]
+    public void TestFetchItem()
+    {
+        string sword = "sword";
+        Item item = inventory.Fetch(sword);
+        bool itemExists = inventory.HasItem(sword);
+        Assert.That(item.FirstId, Is.EqualTo(sword));
+        Assert.That(itemExists, Is.True);
+    }
+
+    [Test(Description = "Returns the item, and the item is no longer in the inventory")]
+    public void TestTakeItem()
+    {
+        string sword = "sword";
+        Item item = inventory.Take(sword);
+        bool itemExists = inventory.HasItem(sword);
+        Assert.That(item.FirstId, Is.EqualTo(sword));
+        Assert.That(itemExists, Is.False);
+    }
+
+    [Test(Description = "Returns a string containing multiple lines. Each line contains a tab-indented short description of an item in the Inventory")]
+    public void TestItemList()
+    {
+        string expected = "\ta shovel (shovel)\r\n\ta sword (sword)\r\n\ta small computer (pc)\r\n";
+        string actual = inventory.ItemList;
+        Assert.That (actual, Is.EqualTo(expected));
+    }
+}
---
+++ SwinAdventureTests/ItemTests.cs
@@ -0,0 +1,39 @@
+﻿using SwinAdventureLibrary;
+
+namespace SwinAdventureTests;
+
+[TestFixture]
+public class ItemTests
+{
+    Item item;
+
+    [SetUp]
+    public void Setup()
+    {
+        string[] idents = new[]{ "sword" };
+        item = new(idents, "a bronze sword", "closed combat weapon");
+    }
+
+    [Test(Description = "The item responds correctly to 'Are You' requests based on the identifiers it is created with")]
+    public void TestItemIdentifiable()
+    {
+        bool result = item.AreYou("sword");
+        Assert.That(result, Is.True);
+    }
+
+    [Test(Description = "The game object's short description returns the string 'a name (first-id)'")]
+    public void TestShortDescriptioin()
+    {
+        string expected = "a bronze sword (sword)";
+        string actual = item.ShortDescription;
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+
+    [Test(Description = "Returns the item's description.")]
+    public void TestFullDescriptioin()
+    {
+        string expected = "closed combat weapon";
+        string actual = item.FullDescription;
+        Assert.That(actual, Is.EqualTo(expected));
+    }
+}
